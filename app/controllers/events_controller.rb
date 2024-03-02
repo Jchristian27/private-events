1e73128dbb8907bfedacaf6d0790a4bfdb42696a
Jonathan Christian <christianj707@gmail.com>
2024-03-02 11:51:06 -0800

    Create page for creating events and update event index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create 
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:event_name, :date, :creator_id)
+    end
 end
