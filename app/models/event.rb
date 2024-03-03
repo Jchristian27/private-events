e6ac72611c69e8ae2b5555c552b193912041a7fc
Jonathan Christian <christianj707@gmail.com>
2024-03-02 16:02:51 -0800

    Fix issues with attendee associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, foreign_key: "creator_id", class_name: "User"
   has_many :event_users
-  has_many :attendees
+  has_many :attendees, through: :event_users, source: :user
 end
