e97b522e104602518ef9aa7eda9f0ea86ed89d88
Jonathan Christian <christianj707@gmail.com>
2024-03-04 14:02:10 -0800

    Fix attend button funcitonality

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, foreign_key: "creator_id", class_name: "User"
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+
+  def attendee_ids
+    self.attendees.map { |attendee| attendee[:id] }
+  end
 end
