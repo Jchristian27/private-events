cabcd19d7be016cdeab5ce9f253d17f1211f8b0c
Jonathan Christian <christianj707@gmail.com>
2024-03-04 16:15:49 -0800

    Refactor using scopes and merge

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,21 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
 
+  scope :past_events, -> { where("date < ?", Date.today) }
+  scope :upcoming_events, -> { where("date > ?", Date.today) }
+
+  scope :previously_attended_events_for_user, ->(user_id) do
+    joins(:attendances)
+      .where(attendances: { attendee_id: user_id })
+      .past_events
+  end
+
+  scope :upcoming_attended_events_for_user, ->(user_id) do
+    joins(:attendances)
+      .where(attendances: { attendee_id: user_id })
+      .upcoming_events
+  end
+
   def attendee_ids
     attendees.map(&:id)
   end
